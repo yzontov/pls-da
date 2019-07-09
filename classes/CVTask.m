1de880ae88ae0cea49c324dc3354a1f53caf0d6d
Yury Zontov <yury.zontov@gmail.com>
2019-07-09 09:23:32 +0300

    cv help in progress

--- classes/CVTask.m
+++ classes/CVTask.m
@@ -116,17 +116,16 @@ classdef CVTask < handle
                     labels = {};
                     for i = 1:length(x)
                         rec = recs(i);
-                        labels = [labels; cellfun(@(x) sprintf('Split %d. %s', i, x), rec.result.Labels, 'UniformOutput', false)];
-                        classes = [classes; cls(self.Splits(:,i) == 1,:)];
-                        classes_train = [classes_train; cls(self.Splits(:,i) == 0,:)];
-                        distances = [distances; rec.result.Distances];
-                        allocation = [allocation; rec.result.AllocationMatrix];
-                        
-%                         if isequal(unique(rec.model.TrainingDataSet.Classes), unique(cls))
-%                             
-%                         else
-%                             
-%                         end
+
+                        if isequal(unique(rec.model.TrainingDataSet.Classes), unique(cls))
+                            labels = [labels; cellfun(@(x) sprintf('Split %d. %s', i, x), rec.result.Labels, 'UniformOutput', false)];
+                            classes = [classes; cls(self.Splits(:,i) == 1,:)];
+                            classes_train = [classes_train; cls(self.Splits(:,i) == 0,:)];
+                            distances = [distances; rec.result.Distances];
+                            allocation = [allocation; rec.result.AllocationMatrix];
+                        else
+                            %TO DO!
+                        end
                         
                     end
 
